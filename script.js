887498bec5c21cc588e234a80636365944291c72
rudinawang <rudnawang07@gmail.com>
2023-12-26 22:24:30 +0545

    logicalCode

---
+++ script.js
@@ -0,0 +1,55 @@
+document.querySelector(".one").addEventListener("click", function () {
+  document.querySelector(".ip").value = "1";
+});
+
+document.querySelector(".two").addEventListener("click", function () {
+  document.querySelector(".ip").value = "2";
+});
+
+document.querySelector(".three").addEventListener("click", function () {
+  document.querySelector(".ip").value = "3";
+});
+
+document.querySelector(".four").addEventListener("click", function () {
+  document.querySelector(".ip").value = "4";
+});
+
+document.querySelector(".five").addEventListener("click", function () {
+  document.querySelector(".ip").value = "5";
+});
+
+document.querySelector(".six").addEventListener("click", function () {
+  document.querySelector(".ip").value = "6";
+});
+
+document.querySelector(".seven").addEventListener("click", function () {
+  document.querySelector(".ip").value = "7";
+});
+
+document.querySelector(".eight").addEventListener("click", function () {
+  document.querySelector(".ip").value = "8";
+});
+
+document.querySelector(".nine").addEventListener("click", function () {
+  document.querySelector(".ip").value = "9";
+});
+
+document.querySelector(".zero").addEventListener("click", function () {
+  document.querySelector(".ip").value = "0";
+});
+
+document.querySelector(".plus").addEventListener("click", function () {
+  document.querySelector(".ip").value = "+";
+});
+
+document.querySelector(".minus").addEventListener("click", function () {
+  document.querySelector(".ip").value = "-";
+});
+s;
+document.querySelector(".into").addEventListener("click", function () {
+  document.querySelector(".ip").value = "*";
+});
+
+document.querySelector(".divide").addEventListener("click", function () {
+  document.querySelector(".ip").value = "/";
+});
